5688dbbfb03136b8db41f86178244ec18edf09f5
admin <admin@Bcalab3-m12>
2022-06-15 09:06:54 +0530

    commit1

---
+++ CaseToolsExam/src/Secret_locker_217.java
@@ -0,0 +1,15 @@
+
+public class Secret_locker_217 {
+	int secret_key;
+	
+	Secret_locker_217(int secret_key) {
+		this.secret_key=secret_key;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
